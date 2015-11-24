e74de488a19a495f3e40acb6480e2ab16e1b07b3
Michael Roitzsch <reactorcontrol@icloud.com>
2015-11-24 12:46:23 +0100

    more robust app name localization
    fall back to English if no translation is available for the desired
    language

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,14 +1,19 @@
 <?php
 
-$name = array(
-	'de' => 'Umfragen',
-	'en' => 'Polls'
+$name_l10n = array(
+	'de'    => 'Umfragen',
+	'de_DE' => 'Umfragen',
+	'en'    => 'Polls',
+	'en_US' => 'Polls'
 );
+
+$language = OCP\Util::getL10N('dudle')->getLanguageCode();
+$name = array_key_exists($language, $name_l10n) ? $name_l10n[$language] : $name_l10n['en'];
 
 OCP\App::addNavigationEntry(array(
 	'id' => 'dudle',
 	'order' => 5,
 	'href' => OCP\Util::linkToRoute('dudle_index'),
 	'icon' => OCP\Util::linkTo('dudle', 'appinfo/app.svg'),
-	'name' => $name[OCP\Util::getL10N('dudle')->getLanguageCode()]
+	'name' => $name
 ));
